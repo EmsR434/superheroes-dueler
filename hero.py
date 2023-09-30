2cae8caf809f3c0fa4f990670bfd7ac28db565ea
EmsR434 <142956103+EmsR434@users.noreply.github.com>
2023-09-30 16:19:54 -0700

    created hero class

---
+++ hero.py
@@ -0,0 +1,21 @@
+import random
+
+
+
+class Hero:
+    def _init_(self, name, starting_hp=100):
+        self.name = name
+        self.starting_hp = starting_hp
+        self.current_hp = starting_hp
+    
+    def fight(self, opponent): 
+        '''current Hero will take turns fighting the opponent hero passed in.
+        '''
+
+
+
+if __name__ == "_main_":
+    my_hero = Hero("Spiderman")
+    opponent = Hero("Storm")
+
+    Hero.fight(opponent)
